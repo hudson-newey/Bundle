0d2678cd73eab5bb2bc71b27e9418235a961c353
Grathium Industries <GrathiumIndustries@gmail.com>
2020-10-18 16:34:30 +1000

    bug fixes with 'bundler commit'

--- src/addons/git.cpp
+++ src/addons/git.cpp
@@ -2,7 +2,9 @@ void gitCommit() {
     // get user specified commit message
     std::cout << "(Commit Message) ";
     std::string commitMessage = "";
-    std::cin >> commitMessage;
+    getline(cin, commitMessage, '\n');
+
+    // modify commit message to commit command
     commitMessage = "git commit -m \"" + commitMessage + "\"";
 
     // default git commit procedures
