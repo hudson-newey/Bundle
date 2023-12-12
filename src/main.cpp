6d1f0613f614d518d0375910cf741e9bd222d508
hudson-newey <grath@duck.com>
2023-12-13 09:03:41 +1000

    Improved cla's

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <unistd.h>
+#include <map>
 
 #include "bundlerInfo.hpp"
 #include "util.cpp"
@@ -13,115 +14,164 @@
 
 #include "addons/addons.hpp"
 
-int main(int argc, char const *argv[])
+enum class Command
 {
-    if (argc > 1)
+    RUN,
+    INIT,
+    CHECK,
+    INSTALL,
+    NEW,
+    RAM,
+
+    // flags
+    HELP,
+    VERSION,
+
+    // addons
+    COMMIT
+};
+
+int main(int argc, const char *argv[])
+{
+    // default is "bundler run" if the program is run without any arguments
+    if (argc == 1)
     {
-        const std::string command = argv[1];
+        bundlerRun();
+        return 0;
+    }
 
-        if (command == "-h" || command == "--help")
+    std::map<std::string, Command> claMap = {
+        {"run", Command::RUN},
+        {"r", Command::RUN},
+        {"init", Command::INIT},
+        {"check", Command::CHECK},
+        {"c", Command::CHECK},
+        {"install", Command::INSTALL},
+        {"i", Command::INSTALL},
+        {"new", Command::NEW},
+        {"n", Command::NEW},
+        {"ram", Command::RAM},
+
+        // flags
+        {"--help", Command::HELP},
+        {"-h", Command::HELP},
+        {"--version", Command::VERSION},
+        {"-v", Command::VERSION},
+
+        // addons
+        {"commit", Command::COMMIT}
+    };
+
+    const std::string command = argv[1];
+
+    switch (claMap[command])
+    {
+    case Command::HELP: {
+        std::cout << BUNDLER_HELP_DOCS;
+        break;
+    }
+
+    case Command::VERSION: {
+        std::cout << BUNDLER_VERSION << "\n";
+        break;
+    }
+
+    case Command::RUN: {
+        if (argc > 2)
         {
-            std::cout << BUNDLER_HELP_DOCS;
-            return 0;
-        }
-        else if (command == "-v" || command == "--version")
-        {
-            std::cout << BUNDLER_VERSION << "\n";
-            return 0;
-        }
-
-        // get CLI
-        if (command == "run" || command == "r")
-        {
-            if (argc > 2) {
-                const std::string templateName = argv[2];
-
-                const std::string username = getlogin();
-                const auto templatePath = "/home/" + username + "/.local/templates/" + templateName + ".yaml";
-
-                BUNDLER_FILE = templatePath;
-            }
-
-            bundlerRun();
-        }
-        else if (command == "init")
-        {
-            bundlerInit();
-        }
-        else if (command == "commit")
-        {
-            gitCommit();
-        }
-        else if (command == "check" || command == "c")
-        {
-            if (argc > 2) {
-                const std::string templateName = argv[2];
-
-                const std::string username = getlogin();
-                const auto templatePath = "/home/" + username + "/.local/templates/" + templateName + ".yaml";
-
-                BUNDLER_FILE = templatePath;
-            }
-
-            verifyAllDependencies();
-        }
-        else if (command == "install" || command == "i")
-        {
-            if (argc > 2) {
-                const std::string templateName = argv[2];
-
-                const std::string username = getlogin();
-                const auto templatePath = "/home/" + username + "/.local/templates/" + templateName + ".yaml";
-
-                BUNDLER_FILE = templatePath;
-            }
-
-            installAllDependencies();
-        }
-        else if (command == "new" || command == "n")
-        {
-
-            if (argc == 2)
-            {
-                printError(1, "Must provide a template name after 'new' command\n\teg. bundler new <template-name>");
-            }
-
             const std::string templateName = argv[2];
-            generateFromTemplate(templateName);
+
+            const std::string username = getlogin();
+            const auto templatePath = "/home/" + username + "/.local/templates/" + templateName + ".yaml";
+
+            BUNDLER_FILE = templatePath;
         }
-        else if (command == "ram")
+
+
+        bundlerRun();
+        break;
+    }
+
+    case Command::INIT: {
+        bundlerInit();
+        break;
+    }
+
+    case Command::CHECK: {
+        if (argc > 2)
         {
+            const std::string checkTemplateName = argv[2];
 
-            if (argc == 2)
-            {
-                printError(1, "Must provide a command after 'ram' command\n\teg. bundler ram <load|unload> <filePath>");
-            }
+            const std::string username = getlogin();
+            const auto templatePath = "/home/" + username + "/.local/templates/" + checkTemplateName + ".yaml";
 
-            const std::string ramCommand = argv[2];
-            if (argc == 3 && ramCommand == "load")
-            {
-                printError(1, "Must provide a file name after 'ram load' command\n\teg. bundler ram <load|unload> <filePath>");
-            }
+            BUNDLER_FILE = templatePath;
+        }
 
-            if (argc == 3 && ramCommand == "unload")
-            {
-                unloadPathFromRam();
-            }
-            else
-            {
-                const std::string fileName = argv[3];
-                ramManipulate(ramCommand, fileName);
-            }
+        verifyAllDependencies();
+        break;
+    }
+
+    case Command::INSTALL: {
+        if (argc > 2)
+        {
+            const std::string installTemplateName = argv[2];
+
+            const std::string username = getlogin();
+            const auto templatePath = "/home/" + username + "/.local/templates/" + installTemplateName + ".yaml";
+
+            BUNDLER_FILE = templatePath;
+        }
+
+        installAllDependencies();
+        break;
+    }
+
+    case Command::NEW: {
+        if (argc <= 2)
+        {
+            printError(1, "Must provide a template name after 'new' command\n\teg. bundler new <template-name>");
+        }
+
+        const std::string newTemplateName = argv[2];
+        generateFromTemplate(newTemplateName);
+        break;
+    }
+
+    case Command::RAM: {
+        if (argc == 2)
+        {
+            printError(1, "Must provide a command after 'ram' command\n\teg. bundler ram <load|unload> <filePath>");
+        }
+
+        const std::string ramCommand = argv[2];
+        if (argc == 3 && ramCommand == "load")
+        {
+            printError(1, "Must provide a file name after 'ram load' command\n\teg. bundler ram <load|unload> <filePath>");
+        }
+
+        if (argc == 3 && ramCommand == "unload")
+        {
+            unloadPathFromRam();
         }
         else
         {
-            printError(1, "Unknown command '" + command + "'");
+            const std::string fileName = argv[3];
+            ramManipulate(ramCommand, fileName);
         }
+        break;
     }
-    else
-    {
-        // default is "bundler run"
-        bundlerRun();
+
+    // addons
+    case Command::COMMIT: {
+        gitCommit();
+        break;
+    }
+
+    default: {
+        printError(1, "Unknown command '" + command + "'");
+        break;
+    }
     }
 
     return 0;
